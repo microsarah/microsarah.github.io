837421bcedeecf4e3109828fb1ef94788046ea0a
sarah hallacher <sarah.hallacher@generalassemb.ly>
2015-11-09 08:12:08 -0500

    removed invibed, ux3 update
    - index finished
    - resize gifs
    - facebook as template
    - about/all
    - mq’s

---
+++ ux3/php/livesearch.php
@@ -0,0 +1,47 @@
+<?php
+
+$xmlDoc=new DOMDocument();
+$xmlDoc->load("../_xml/links.xml");
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+$x=$xmlDoc->getElementsByTagName('link');
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('title');
+    $z=$x->item($i)->getElementsByTagName('url');
+    if ($y->item(0)->nodeType==1) {
+    
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint="<a href='" . 
+          $z->item(0)->childNodes->item(0)->nodeValue . 
+          "' target='_self'>" . 
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        } else {
+          $hint=$hint . "<br /><a href='" . 
+          $z->item(0)->childNodes->item(0)->nodeValue . 
+          "' target='_self'>" . 
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        }
+      }
+    }
+  }
+}
+
+// Set output to "no suggestion" if no hint were found
+// or to the correct values
+if ($hint=="") {
+  $response="did you mean 'i am heartbroken'?";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
+?>
