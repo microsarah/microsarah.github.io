45716ee609c3612ed5415792c750466e50aa58c9
microsarah <sarah.hallacher@gmail.com>
2016-09-29 21:31:21 -0400

    JSD week 2 homework

--- jsd920_class_repo/week2/homework/99_bottles_of_beer.js
+++ jsd920_class_repo/week2/homework/99_bottles_of_beer.js
@@ -3,10 +3,16 @@
 // Make sure your program can handle both singular and plural cases (i.e. both "100 bottles of beer" and "1 bottle of beer")
 // Hint: you will want to use a for loop that starts at 99 and counts down to 0
 
-for(i = 99; i > 0; i--){
+var numBottles = 99;
+
+for(i = numBottles; i >= 0; i--){
 	if(i > 2){
 		console.log(i + " bottles of beer on the wall, " + i + " bottles of beer! Take one down, pass it around, " + (i - 1) + " bottles of beer on the wall!");
 	} else if(i === 2){
 		console.log(i + " bottles of beer on the wall, " + i + " bottles of beer! Take one down, pass it around, " + (i - 1) + " bottle of beer on the wall!");
+	} else if(i === 1){
+		console.log(i + " bottle of beer on the wall, " + i + " bottle of beer! Take one down, pass it around, no more bottles of beer on the wall!");
+	} else {
+		console.log("No more bottles of beer on the wall, no more bottles of beer! Go to the store and buy some more, " + numBottles + " bottles of beer on the wall!");
 	}
 }
--- jsd920_class_repo/week2/homework/readme.md
+++ jsd920_class_repo/week2/homework/readme.md
@@ -2,10 +2,16 @@
 
 ### Assignment Due on Thursday, 9/29:
 
+#### Read the following articles:
+
+* [JS Object Reference](http://www.w3schools.com/jsref/jsref_operators.asp)
+* [The for loop vs forEach](http://thejsguy.com/2016/07/30/javascript-for-loop-vs-array-foreach.html)
+* [for vs while Performance](http://www.stoimen.com/blog/2012/01/24/javascript-performance-for-vs-while/)
+
 #### Optional
 * Continue reading chapter(s) 1,2 of [Eloquent JS] (http://eloquentjavascript.net/)
 
-### Assignment Due on Tuesday, 9/27:
+### Assignment Due on Tuesday, 10/4:
 
 #### Mandatory
 
