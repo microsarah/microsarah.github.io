93fa123253f8e79f53cb52b9cb15861f7e7a9623
microsarah <sarah.hallacher@gmail.com>
2016-11-26 14:08:00 -0500

    consolidated functions, added fonts

--- jsd920_class_repo/projects/final_project/script.js
+++ jsd920_class_repo/projects/final_project/script.js
@@ -21,20 +21,71 @@ $(document).ready( function(){
 		dec : 31,
 	};
 
-	// new workflow:
 
-	// ----------------------------------------------- search a day
-	// ----------------------------------------------- get the video id
-	// ----------------------------------------------- save the video id to an array
+	// start it off
+	currentMonth = 'january';
+	searchForVid();
+
+	// search for videos on a 10-sec timer
+	for (var i = totalDays; i <= year; i++){
+		setTimeout(searchForVid, (10000 * i));
+	}
+
+
+
+
+	
+	// ----------------------------------------------- set the current month, then
+	// ----------------------------------------------- query data using current month & day
+	function searchForVid(){
+		setMonth();
+		queryData(currentMonth, currentDay);
+	}
+
+	// ----------------------------------------------- call the API 
+	// ----------------------------------------------- if it works, run the success function
+	// ----------------------------------------------- if it fails, restart search immediately
+	function queryData(month,day){
+		var url = (baseURL + month + '-' + day +'-' + 'birthday');
+		$.get(url).done(onSuccess).fail(searchForVid); 
+	}
+	
+	// ----------------------------------------------- if the query is successful, save the data
+	function onSuccess(res) {
+		console.log(res)
+
+		// ----------------------------------------------- get the video id
+		// ----------------------------------------------- save the video id to an array
+		var totalItems = res.items.length;
+		var randomVid = Math.floor(Math.random() * totalItems);
+		allVideos.push(res.items[randomVid].id.videoId);
+		renderData(res);
+	}
+
 	// ----------------------------------------------- render the video using the video id from the array
-	// ----------------------------------------------- start timer
-	// ----------------------------------------------- after 5 seconds, do the next search
-	// ----------------------------------------------- after 10 seconds, repeat this loop
+	function renderData(){
+		var id = allVideos[totalDays - 1];
+		$('.video-container').append('<iframe src="https://www.youtube.com/embed/' + id + '?autoplay=1;?t=1m1s;rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>');
+		
+		// ----------------------------------------------- add a 0 to single-digit dates
+		if(currentDay < 10){
+			$('.title').append('<h1>' + monthNum + '/0' + currentDay + '/' + '2016</h1>');
+		} else {
+			$('.title').append('<h1>' + monthNum + '/' + currentDay + '/' + '2016</h1>');
+		}
+		
+		// ----------------------------------------------- remove previous iframe after initial iframe renders
+		if (totalDays > 1){
+			$('iframe:first-of-type').remove();
+			$('h1:first-of-type').remove();
+		}
 
-	// searchAll runs queryData
-	// queryData runs saveData
-	// saveData runs renderData
+		currentDay++;
+		totalDays++;
+	}
 
+	// ----------------------------------------------- set the currentMonth 
+	// ----------------------------------------------- reset the currentDay after the month ends
 	function setMonth(){
 		if (currentMonth === 'january' && currentDay > NumDays.jan){
 			currentDay = 1;
@@ -75,125 +126,29 @@ $(document).ready( function(){
 		}
 
 		if (currentMonth === 'january'){
-			monthNum = 01;
+			monthNum = '01';
 		} else if (currentMonth === 'february'){
-			monthNum = 02;
+			monthNum = '02';
 		} else if (currentMonth === 'march'){
-			monthNum = 03;
+			monthNum = '03';
 		} else if (currentMonth === 'april'){
-			monthNum = 04;
+			monthNum = '04';
 		} else if (currentMonth === 'may'){
-			monthNum = 05;
+			monthNum = '05';
 		} else if (currentMonth === 'june'){
-			monthNum = 06;
+			monthNum = '06';
 		} else if (currentMonth === 'july'){
-			monthNum = 07;
+			monthNum = '07';
 		} else if (currentMonth === 'august'){
-			monthNum = 08;
+			monthNum = '08';
 		} else if (currentMonth === 'september'){
-			monthNum = 09;
+			monthNum = '09';
 		} else if (currentMonth === 'october'){
-			monthNum = 10;
+			monthNum = '10';
 		} else if (currentMonth === 'november'){
-			monthNum = 11;
+			monthNum = '11';
 		} else if (currentMonth === 'december'){
-			monthNum = 12;
+			monthNum = '12';
 		}      
 	}
-
-
-	// start it off
-	currentMonth = 'january';
-	searchAll();
-
-	// search for videos on a 10-sec timer
-
-
-	// ---------------------------------------- this code renders way too fast
-	for (var i = totalDays; i <= 365; i++){
-		setTimeout(searchAll, (10000 * i));
-	}
-	
-	// idea: could i do 6 days at a time (1 minute of material)
-
-
-	function searchAll(){
-		//console.log('total days counter start: ' + totalDays)
-		//console.log('day counter start: ' + currentDay)
-
-		// ----------------------------------------------- set the month based on the totalDays counter
-		setMonth();
-		queryData(currentMonth, currentDay);
-	}
-
-
-	function queryData(month,day){
-		var url = (baseURL + month + '-' + day +'-' + 'birthday');
-		console.log(url);
-		$.get(url).done(onSuccess).fail(onFail);
-		//console.log(month, day)
-	}
-	
-	// if the query is successful, then run saveData() 
-	function onSuccess(res) {
-		console.log(res)
-		saveData(res);
-	}
-
-
-	function saveData(data){
-
-		// ----------------------------------------------- get the video id
-		// ----------------------------------------------- save the video id to an array
-		var totalItems = data.items.length;
-		var randomVid = Math.ceil(Math.random() * totalItems);
-		//console.log('total items: ' + totalItems);
-
-		allVideos.push(data.items[randomVid].id.videoId);
-
-		//console.log(allVideos);
-		renderData(data);
-	}
-
-
-	function renderData(){
-		// ----------------------------------------------- render the video using the video id from the array
-		var id = allVideos[totalDays - 1];
-		
-		//console.log(id)
-		$('.video-container').append('<iframe src="https://www.youtube.com/embed/' + id + '?autoplay=1;?t=1m1s;rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>');
-		
-		if(currentDay < 10){
-			$('.title').append('<h1>' + monthNum + '/0' + currentDay + '/' + '2016</h1>');
-		} else {
-			$('.title').append('<h1>' + monthNum + '/' + currentDay + '/' + '2016</h1>');
-		}
-		
-
-		// ----------------------------------------------- remove previous iframe after initial iframe renders
-		if (totalDays > 1){
-			//console.log('removing iframe');
-			$('iframe:first-of-type').remove();
-			$('h1:first-of-type').remove();
-		}
-
-		currentDay++;
-		totalDays++;
-		
-		//console.log('total days counter end: ' + totalDays);
-	}
-
-
-	function onFail(err){
-		//console.log('Error accessing data: '+ err);
-	}
-
 });
-
-
-
-
-// search all for 1 month in 10s increments
-// add an array of objects called january
-//
-
